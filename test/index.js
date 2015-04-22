4a7cbca6e0a12b68aa6071dbfd627c358c78025d
Paul C Pederson <ppederson@esri.com>
2015-04-21 18:00:49 -0700

    stub out all tests

---
+++ test/index.js
@@ -0,0 +1,43 @@
+var test = require('tape')
+var ghReleaseAssets = require('../')
+
+test('should upload an asset in string format', function (t) {
+  t.plan(1)
+})
+
+test('should upload an asset in object format', function (t) {
+  t.plan(1)
+})
+
+test('should properly rename an asset in object format', function (t) {
+  t.plan(1)
+})
+
+test('should emit `upload-asset` event', function (t) {
+  t.plan(1)
+})
+
+
+test('should emit `upload-progress` events', function (t) {
+  t.plan(1)
+})
+
+test('should emit `uploaded-asset` event', function (t) {
+  t.plan(1)
+})
+
+test('should return an array of the uploaded files', function (t) {
+  t.plan(1)
+})
+
+test('should return an error for missing files', function (t) {
+  t.plan(1)
+})
+
+test('should return an error if there is no token', function (t) {
+  t.plan(1)
+})
+
+test('should return an error if there is no token', function (t) {
+  t.plan(1)
+})
