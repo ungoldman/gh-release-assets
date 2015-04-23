f04d8116662326e013e36326b1a98e93012185eb
Paul C Pederson <paul.c.pederson@gmail.com>
2015-04-22 21:50:04 -0700

    improve error handling

--- index.js
+++ index.js
@@ -14,16 +14,24 @@ function Upload () {
   var cb = this.cb
 
   if (!opts.assets || opts.assets.length === 0) return cb([])
-
+  if (!opts.token) return cb(new Error('You must define a valid GitHub user token as `token`.'))
   var files = []
 
   async.eachSeries(opts.assets, function (asset, callback) {
     var fileName = asset.name || path.basename(asset)
     asset = asset.path || asset
     var uploadUri = opts.url.split('{')[0] + '?name=' + fileName
+
+    var stat
+
+    try {
+      stat = fs.statSync(asset)
+    } catch (e) {
+      return cb(e)
+    }
+
     self.emit('upload-asset', fileName)
 
-    var stat = fs.statSync(asset)
     var rd = fs.createReadStream(asset)
     var us = request({
       method: 'POST',
@@ -32,7 +40,7 @@ function Upload () {
         'Authorization': 'token ' + opts.token,
         'Content-Type': mime.lookup(fileName),
         'Content-Length': stat.size,
-        'User-Agent': 'publish-release ' + pkg.version + ' (https://github.com/paulcpederson/gh-release-assets)'
+        'User-Agent': 'gh-release-assets ' + pkg.version + ' (https://github.com/paulcpederson/gh-release-assets)'
       }
     })
 
@@ -62,10 +70,11 @@ function Upload () {
 
 function UploadAsset (opts, cb) {
   var Uploader = extend(new Emitter(), {upload: Upload})
-  Uploader.opts = opts || {}
-  Uploader.cb = cb || function noop () {}
-  Uploader.upload()
-  return Uploader
+  var uploader = Object.create(Uploader)
+  uploader.opts = opts || {}
+  uploader.cb = cb || function noop () {}
+  uploader.upload()
+  return uploader
 }
 
 module.exports = UploadAsset
